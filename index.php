04efc04597aaa1cd024d6a22fb12c47f45aea29f
429273-4 <429273-4>
2020-11-05 10:02:41 +0300

    second commit

--- index.php
+++ index.php
@@ -1,17 +1,43 @@
 <?php
-class A {}
-class B extends A {
-	protected $a;
-	protected $b;
-	public function __construct($a,$b){
-		$this->a = $a;
-		$this->b = $b;
+Class A
+{
+	public function lin($a, $b)
+	{
+		if($a==0)
+		{
+			return null;
+		}
+		$x=-($b/(2*$a));
+		return $x;
 	}
 }
-$a1=new A();
-$a2=new A();
-$a3=new A();
-$b4=new B($a2,$a3);
-$b5=new B($a1,$b4);
-var_dump($b5);
+Class B extends A
+{
+	protected function dis($a, $b, $c)
+	{
+		$d=($b*$b-(4*$a*$c));
+		return $d;
+	}
+	public function korni($a, $b, $c)
+	{
+		if ($a==0)
+		{
+			return $this->lin($b,$c);
+		}
+		$d=$this->dis($a, $b, $c);
+		if($d>0)
+		{
+			$d=sqrt($d);
+			return $this->x=array((-$b+$d)/(2*$a),(-$b-$d)/(2*$a));
+		}
+		if($d==0)
+		{
+			$x=(-($b)/(2*$a));
+			return $this->x=array((-$b+$d)/(2*$a));
+		}
+	}
+}
+$a=new A();
+$b=new B();
+var_dump($b->korni(2,5,2));
 ?>
